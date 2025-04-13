dbfeda5f7b00fbdb601a9969a2ad24fb8068c44d
Henrique Ribeiro <henriqueglribeiro@gmail.com>
2025-04-13 18:08:39 -0300

    CONJUNÇÃO DIVINA
    O JUIZO FINAL

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
- window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    if (window.scrollY > 50) {
-      navbar.classList.add("shrink");
-    } else {
-      navbar.classList.remove("shrink");
-    }
-  });
+// Efeito de redução no tamanho da navbar ao rolar a página
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("shrink");
+  } else {
+    navbar.classList.remove("shrink");
+  }
+});
